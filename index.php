f6baa385bd6e3cb7e6e524bfdcc9eca9a0fc026e
g118 <gurpreetmas@gmail.com>
2018-01-02 10:33:55 -0500

    commit

---
+++ index.php
@@ -0,0 +1,285 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Gurpreet Singh Portfolio</title>
+    <link rel="icon" type="img/png" href="images/favicon.ico" sizes="16x16">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Website Template by FreeHTML5.co" />
+	<meta name="keywords" content="free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+	<meta name="author" content="FreeHTML5.co" />
+
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body style="background-image: url(images/bg_img.jpg);">
+
+	<div id="fh5co-main">
+		<div class="fh5co-tab-wrap">
+			<ul class="fh5co-tab-menu">
+				<li class="active"><a href="#" data-tab="1"><span class="icon icon-glass"></span><span class="menu-text">Profile</span></a></li>
+				<li><a href="#" data-tab="2"><span class="icon icon-graduation-cap"></span><span class="menu-text">Education</span></a></li>
+				<li><a href="#" data-tab="3"><span class="icon icon-black-tie"></span><span class="menu-text">Experience & Projects</span></a></li>
+				<li><a href="#" data-tab="4" data-pie="yes"><span class="icon icon-fighter-jet"></span><span class="menu-text">Skills</span></a>
+                <li><a href="#" data-tab="5" data-pie="yes"><span class="icon icon-mobile-phone"></span><span class="menu-text">Contact</span></a>
+                </li>
+			</ul>
+			<div class="fh5co-tab-content active" data-content="1">
+				<div class="fh5co-content-inner text-center">
+					<div class="row row-bottom-padded-sm">
+						<div class="col-md-12">
+							<figure>
+								<img class="img-circle" src="images/picmy.png" alt="Gurpreet">
+							</figure>
+							<h1>Gurpreet Singh</h1>
+							<p>Dynamic and passionate Software Development Professional Seeking a full time position.Where i can
+                             Apply my technical knowledge</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12 fh5co-counter">
+							<div class="fh5co-number fh5co-left">3</div>
+							<div class="fh5co-text">
+								<h3 class="border-bottom">Years of experience in managing and providing Software Support to KFC/Taco Bell</h3>
+								<p>Providing Software Support for clearview Software, Managing inventory , generating daily financial reports, scheduling, making payrolls and providing training to staff on using software</p>
+							</div>
+						</div>
+					</div>
+					<ul class="fh5co-social">
+						<li><a href="mailto:gurpreetmas@gmail.com"><i class="icon-envelope"></i></a></li>
+						<li><a href="https://github.com/g118" target="_blank"><i class="icon-github"></i></a></li>
+						<li><a href="https://ca.linkedin.com/in/gurpreet-singh-4b12b818" target="_blank"><i class="icon-linkedin"></i></a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="fh5co-tab-content" data-content="2">
+				<div class="fh5co-content-inner">
+					<div class="row">
+						<div class="col-md-12">
+							<p>Reflection of my Education Credentials.</p>
+
+							<div class="fh5co-feature">
+								<div class="fh5co-icon">
+									<i class="icon-graduation-cap"></i>
+								</div>
+								<div class="fh5co-text">
+									<h2>2016-Present</h2>
+									<p>Computer Application Development Certification Program(Co-op).</p>
+                                    <p>Conestoga College, Kitchener, ON</p>
+								</div>
+							</div>
+
+							<div class="fh5co-feature">
+								<div class="fh5co-icon">
+									<i class="icon-graduation-cap"></i>
+								</div>
+								<div class="fh5co-text">
+									<h2>2012-2012</h2>
+									<p>International Business Management Certification Programm.</p>
+                                    <p>Centennial College, Scarborough, ON</p>
+								</div>
+							</div>
+
+							<div class="fh5co-feature">
+								<div class="fh5co-icon">
+									<i class="icon-graduation-cap"></i>
+								</div>
+								<div class="fh5co-text">
+									<h2>2006-2010</h2>
+									<p>Bachelors Of Technology(Electronics and Communication).</p>
+                                    <p>Punjab Technical University, Punjab, India</p>
+								</div>
+							</div>
+
+						</div>
+
+					</div>
+				</div>
+			</div>
+			<div class="fh5co-tab-content" data-content="3">
+				<div class="fh5co-content-inner">
+					<div class="row">
+						<div class="col-md-12">
+
+							<p>I have the privilege to work with these cool companies.</p>
+
+							<div class="fh5co-feature">
+								<div class="fh5co-icon">
+									<i class="icon-black-tie"></i>
+								</div>
+								<div class="fh5co-text">
+									<h2>2013 - 2016</h2>
+									<h3>KFC/Taco Bell</h3>
+									<p>Managed and provided techncial support to the restaurant on the clearview POS software,
+                                    Performed Inventory Managment , Payroll , generating financial reports and provided training to staff on Clearview software</p>
+								</div>
+							</div>
+
+							<div class="fh5co-feature">
+								<div class="fh5co-icon">
+									<i class="icon-black-tie"></i>
+								</div>
+								<div class="fh5co-text">
+									<h2>2012-2013</h2>
+									<h3>Mobilicity</h3>
+									<p>Worked on ERP Software for generating invoices, recieving purchase order and did inventory Managemnt.
+                                    Provided technical support to customers.</p>
+								</div>
+							</div>
+
+							<div class="fh5co-feature">
+								<div class="fh5co-icon">
+									<i class="icon-black-tie"></i>
+								</div>
+								<div class="fh5co-text">
+									<h2>2010-2012</h2>
+									<h3>Industrial Development Corporation</h3>
+									<p>Worked on Computer Aided Design Software for generating drawings for Autoparts and provided quality assurance support to maintain quality of products.</p>
+								</div>
+							</div>
+
+                            <h3>Worked in the following project.</h3>
+
+                            <div class="fh5co-feature">
+								<div class="fh5co-icon">
+									<i class="icon-graduation-cap"></i>
+								</div>
+								<div class="fh5co-text">
+									<h2>Capstone Project</h2>
+									<h3>Leader Building</h3>
+									<p>Can not discuss Client's Project in detail as of Intellectual property Norms.Technologies associated with this project are Frontend - Javascript, HTML, CSS, Backend- ASP.Net MVC Framework</p>
+								</div>
+							</div>
+
+						</div>
+
+					</div>
+				</div>
+			</div>
+			<div class="fh5co-tab-content" data-content="4">
+				<div class="fh5co-content-inner">
+					<div class="row">
+						<div class="col-md-12">
+							<p>Skills Acquired through Work expericne and School.</p>
+						</div>
+						<div class="col-md-4 col-sm-6 col-xs-12 text-center">
+							<div class="chart" data-percent="95"><span><strong>C#</strong>95%</span></div>
+						</div>
+						<div class="col-md-4 col-sm-6 col-xs-12 text-center">
+							<div class="chart" data-percent="93"><span><strong>Javascript</strong>90%</span></div>
+						</div>
+						<div class="col-md-4 col-sm-6 col-xs-12 text-center">
+							<div class="chart" data-percent="90"><span><strong>HTML & CSS</strong>90%</span></div>
+						</div>
+
+						<div class="col-md-4 col-sm-6 col-xs-12 text-center">
+							<div class="chart" data-percent="89"><span><strong>ASP.Net</strong>85%</span></div>
+						</div>
+						<div class="col-md-4 col-sm-6 col-xs-12 text-center">
+							<div class="chart" data-percent="85"><span><strong>MySQL</strong>90%</span></div>
+						</div>
+						<div class="col-md-4 col-sm-6 col-xs-12 text-center">
+							<div class="chart" data-percent="90"><span><strong>AngularJS</strong>90%</span></div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+
+            			<div class="fh5co-tab-content" data-content="5">
+				<div class="fh5co-content-inner">
+					<div class="row">
+						<div class="col-md-12">
+
+							<p>If you find my work interesting, contact me:</p>
+
+							<div class="fh5co-feature">
+								<div class="fh5co-icon">
+									<i class="icon-mobile"></i>
+								</div>
+								<div class="fh5co-text">
+									<h2>Give me a Call</h2>
+									<h3>@647-761-3843</h3>
+                                    <p></p>
+								</div>
+							</div>
+
+							<div class="fh5co-feature">
+								<div class="fh5co-icon">
+									<i class="icon-envelope"></i>
+								</div>
+								<div class="fh5co-text">
+									<h2>Email me</h2>
+									<h3>gurpreetmas@gmail.com</h3>
+									<p></p>
+								</div>
+							</div>
+
+							<div class="fh5co-feature">
+								<div class="fh5co-icon">
+									<i class="icon-home"></i>
+								</div>
+								<div class="fh5co-text">
+									<h2>Address</h2>
+									<h3>235-B Cedarbrae Avenue, <br> Waterloo, ON N2L 4S8</h3>
+									<p></p>
+								</div>
+							</div>
+
+						</div>
+
+					</div>
+				</div>
+			</div>
+
+		</div>
+		<footer id="fh5co-footer">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 text-center">
+					<small>
+						&copy; 2016 Gurpreet Singh. All Rights Reserved.
+					</small>
+				</div>
+			</div>
+		</footer>
+	</div>
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Easy PieChart -->
+	<script src="js/jquery.easypiechart.min.js"></script>
+	<!-- MAIN JS -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
